780c02c706c2200bf9e9958f61ee99e1de69966c
Free Beachler <fbeachler@gmail.com>
2012-11-03 02:56:31 -0600

    create gh-pages branch;

--- tools/build/ant_assembly.scad.tpl
+++
@@ -1,5 +0,0 @@
-// SCAD assembly template for ANT
-// auto-generated - DO NOT edit manually!
-
-use <!scad.filename!>
-!scad.call!
--- tools/build/build_all_parts.sh
+++
@@ -1,6 +0,0 @@
-ant build-filtered-assembly-scad -DprofileId=5mmsterm
-ant build-filtered-assembly-scad -DprofileId=5mmsterm_clip
-ant build-filtered-assembly-scad -DprofileId=ramps14_noclip
-ant build-filtered-assembly-scad -DprofileId=ramps14_clip
-ant build-filtered-assembly-scad -DprofileId=atxpsubrd_noclip
-ant build-filtered-assembly-scad -DprofileId=atxpsubrd_clip
