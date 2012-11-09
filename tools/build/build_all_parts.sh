7e9a0b1832598a58112d80c80e81217aa6c39c2d
Free Beachler <fbeachler@gmail.com>
2012-11-09 05:53:34 -0700

    added build profile and distro for mnt_psu;

--- tools/build/build_all_parts.sh
+++ tools/build/build_all_parts.sh
@@ -4,3 +4,4 @@ ant build-filtered-assembly-scad -DprofileId=ramps14_noclip
 ant build-filtered-assembly-scad -DprofileId=ramps14_clip
 ant build-filtered-assembly-scad -DprofileId=atxpsubrd_noclip
 ant build-filtered-assembly-scad -DprofileId=atxpsubrd_clip
+ant build-filtered-assembly-scad -DprofileId=miniatxpsu
